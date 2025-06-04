4fa426b1fe8724da5adb745eb96064e3b1b02fbe
JoseeGarciaa <113802219+JoseeGarciaa@users.noreply.github.com>
2025-06-04 18:27:39 -0500

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'admin') {
-  header("Location: ../views/login.html");
+  header("Location: ../views/login.php");
   exit();
 }
 
